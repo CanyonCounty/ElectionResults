8cace7071aeb102664ae4a54a8c55b576659ee9b
Kenneth Wilcox <wilcoxk@gmail.com>
2014-11-21 16:53:19 -0700

    starting on details

--- scripts/script.js
+++ scripts/script.js
@@ -24,5 +24,28 @@ $(document).ready(function(event) {
     }
   });
   
+  $.ajax("http://ccmtprod08.canyonco.org/Election/ElectionResultDetails", {
+    success: function(data) {
+      if (!data.Error) {
+        $.each(data.Results, function(index, row) {
+          if (row.PartyName)
+            alert(row.PartyName);
+          else {
+            $.each(row.Contests, function(ci, crow) {
+              $('<input/>', {'value':JSON.stringify(crow), attr:{size: "170"}}).appendTo("#electionDetails");
+            });
+          }
+        });
+      } else {
+        alert("Result Details Error: " + data.Error);
+        $(".electionContainer").hide();
+      }
+    },
+    error: function(err) {
+      alert("Error: " + err.status + " " + err.statusText + " " + err.responseText);
+      $(".electionContainer").hide();
+    }
+  });
+  
   // More tk...
 });
