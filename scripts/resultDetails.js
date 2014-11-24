b8c10be21746a3d648d43db3c88fae04b251c510
Kenneth Wilcox <wilcoxk@gmail.com>
2014-11-24 10:33:28 -0700

    renamed files

--- scripts/resultDetails.js
+++ scripts/resultDetails.js
@@ -3,27 +3,6 @@
 /* global alert:false */
 
 $(document).ready(function(event) {
-  // Election Results - Summary
-  $.ajax("http://ccmtprod08.canyonco.org/Election/ResultSummary", {
-    success: function(data) {
-      if (!data.Error) {
-        $("#election").text(data.ElectionName + ' - ' + data.ElectionDate);
-        $("#updated").text(data.LastUpdated);
-        $("#voting").text(data.TotalVoters);
-        $("#counted").text(data.BallotsCounted);
-        $("#turnout").text(data.VoterTurnout);
-        $("#complete").html('<span id="totalcounted">' + data.PrecinctsComplete.replace(' ', '</span> '));
-      } else {
-        alert("Result Summary Error: " + data.Error);
-        $(".electionContainer").hide();
-      }
-    },
-    error: function(err) {
-      alert("Error: " + err.status + " " + err.statusText + " " + err.responseText);
-      $(".electionContainer").hide();
-    }
-  });
-  
   $.ajax("http://ccmtprod08.canyonco.org/Election/ElectionResultDetails", {
     success: function(data) {
       if (!data.Error) {
@@ -47,5 +26,4 @@ $(document).ready(function(event) {
     }
   });
   
-  // More tk...
 });
---
+++ scripts/resultSummary.js
@@ -0,0 +1,26 @@
+/* global $:false */
+/* global document:false */
+/* global alert:false */
+
+$(document).ready(function(event) {
+  // Election Results - Summary
+  $.ajax("http://ccmtprod08.canyonco.org/Election/ResultSummary", {
+    success: function(data) {
+      if (!data.Error) {
+        $("#election").text(data.ElectionName + ' - ' + data.ElectionDate);
+        $("#updated").text(data.LastUpdated);
+        $("#voting").text(data.TotalVoters);
+        $("#counted").text(data.BallotsCounted);
+        $("#turnout").text(data.VoterTurnout);
+        $("#complete").html('<span id="totalcounted">' + data.PrecinctsComplete.replace(' ', '</span> '));
+      } else {
+        alert("Result Summary Error: " + data.Error);
+        $(".electionContainer").hide();
+      }
+    },
+    error: function(err) {
+      alert("Error: " + err.status + " " + err.statusText + " " + err.responseText);
+      $(".electionContainer").hide();
+    }
+  });
+});
